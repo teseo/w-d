795154197935382a21ae597890020572e282c550
teseo <sol@javiermellado.com>
2016-10-30 01:16:45 +0100

    version 2 and all containers connected

--- src/index.php
+++ src/index.php
@@ -1,26 +1,13 @@
 <?php
-// connect
-$m = new MongoDB\Driver\Manager("mongodb://mongo:27017");
-var_dump($m);
 
-// select a database
-$db = $m->comedy;
-var_dump($db);
-// select a collection (analogous to a relational database's table)
-$collection = $db->cartoons;
+require_once 'vendor/autoload.php';
+// Parameters passed using a named array:
+$client = new Predis\Client([
+    'scheme' => 'tcp',
+    'host'   => 'redis',
+    'port'   => 6379,
+]);
 
-// add a record
-$document = array( "title" => "Calvin and Hobbes", "author" => "Bill Watterson" );
-$collection->insert($document);
-
-// add another record, with a different "shape"
-$document = array( "title" => "XKCD", "online" => true );
-$collection->insert($document);
-
-// find everything in the collection
-$cursor = $collection->find();
-
-// iterate through the results
-foreach ($cursor as $document) {
-    echo $document["title"] . PHP_EOL;
-}
+//$client->set('foo', 'bar');
+$value = $client->get('foo');
+var_dump($value);
